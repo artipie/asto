5f355ac105980eec8a26c88eee258646bf3b5508
Oleg Mozzhechkov <oleg.mozzhechkov@gmail.com>
2020-08-10 10:34:57 +0300

    #214 - Added Lock and StorageLock with acquiring

---
+++ src/main/java/com/artipie/asto/lock/Lock.java
@@ -0,0 +1,48 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2020 artipie.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.artipie.asto.lock;
+
+import java.util.concurrent.CompletionStage;
+
+/**
+ * Asynchronous lock that might be successfully obtained by one thread only at a time.
+ *
+ * @since 0.24
+ */
+public interface Lock {
+
+    /**
+     * Acquire the lock.
+     *
+     * @return Completion of lock acquire operation.
+     */
+    CompletionStage<Void> acquire();
+
+    /**
+     * Release the lock.
+     *
+     * @return Completion of lock release operation.
+     */
+    CompletionStage<Void> release();
+}
---
+++ src/test/java/com/artipie/asto/lock/StorageLockTest.java
@@ -0,0 +1,84 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2020 artipie.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.artipie.asto.lock;
+
+import com.artipie.asto.Content;
+import com.artipie.asto.Key;
+import com.artipie.asto.memory.InMemoryStorage;
+import java.util.concurrent.CompletionException;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.collection.IsEmptyCollection;
+import org.hamcrest.core.IsNot;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+
+/**
+ * Test cases for {@link StorageLock}.
+ *
+ * @since 0.24
+ */
+@Timeout(1)
+final class StorageLockTest {
+
+    /**
+     * Storage used in tests.
+     */
+    private final InMemoryStorage storage = new InMemoryStorage();
+
+    /**
+     * Lock target key.
+     */
+    private final Key target = new Key.From("a/b/c");
+
+    @Test
+    void shouldAddValueWhenAcquiredLock() {
+        new StorageLock(this.storage, this.target).acquire().join();
+        MatcherAssert.assertThat(
+            this.storage.list(new StorageLock.ProposalsKey(this.target))
+                .toCompletableFuture().join(),
+            new IsNot<>(new IsEmptyCollection<>())
+        );
+    }
+
+    @Test
+    void shouldAcquireSameLockTwice() {
+        final StorageLock lock = new StorageLock(this.storage, this.target);
+        lock.acquire().join();
+        lock.acquire().join();
+    }
+
+    @Test
+    void shouldFailAcquireLockIfOtherProposalExists() {
+        this.storage.save(
+            new Key.From(new StorageLock.ProposalsKey(this.target), "123"),
+            new Content.From(new byte[] {})
+        ).toCompletableFuture().join();
+        final StorageLock lock = new StorageLock(this.storage, this.target);
+        Assertions.assertThrows(
+            CompletionException.class,
+            () -> lock.acquire().join()
+        );
+    }
+}
