49e0ce5f49559cc3ae765d35900f1c04991a654e
Oleg Mozzhechkov <oleg.mozzhechkov@gmail.com>
2020-08-18 19:48:11 +0300

    #231 - Using expiration time when acquiring StorageLock

--- src/main/java/com/artipie/asto/lock/storage/Proposals.java
+++ src/main/java/com/artipie/asto/lock/storage/Proposals.java
@@ -26,6 +26,7 @@ package com.artipie.asto.lock.storage;
 import com.artipie.asto.Content;
 import com.artipie.asto.Key;
 import com.artipie.asto.Storage;
+import com.artipie.asto.ext.PublisherAs;
 import java.nio.charset.StandardCharsets;
 import java.time.Instant;
 import java.util.Optional;
@@ -84,23 +85,44 @@ final class Proposals {
      * @return Completion of proposals check operation.
      */
     public CompletionStage<Void> checkSingle(final String uuid) {
-        final Key expected = this.proposalKey(uuid);
+        final Key own = this.proposalKey(uuid);
         return this.storage.list(new RootKey(this.target)).thenCompose(
-            proposals -> {
-                if (proposals.size() != 1 || !proposals.contains(expected)) {
-                    throw new IllegalStateException(
-                        String.format(
-                            "Failed to acquire lock. Own: `%s` Found: %s",
-                            expected,
-                            proposals.stream()
-                                .map(Key::toString)
-                                .map(str -> String.format("`%s`", str))
-                                .collect(Collectors.joining(", "))
-                        )
-                    );
-                }
-                return CompletableFuture.allOf();
-            }
+            proposals -> CompletableFuture.allOf(
+                proposals.stream()
+                    .filter(key -> !key.equals(own))
+                    .map(
+                        proposal -> this.storage.value(proposal)
+                            .thenApply(PublisherAs::new)
+                            .thenCompose(PublisherAs::asciiString)
+                            .thenCompose(
+                                expiration -> {
+                                    if (isNotExpired(expiration)) {
+                                        throw new IllegalStateException(
+                                            String.join(
+                                                "\n",
+                                                "Failed to acquire lock.",
+                                                String.format("Own: `%s`", own),
+                                                String.format(
+                                                    "Others: %s",
+                                                    proposals.stream()
+                                                        .map(Key::toString)
+                                                        .map(str -> String.format("`%s`", str))
+                                                        .collect(Collectors.joining(", "))
+                                                ),
+                                                String.format(
+                                                    "Not expired: `%s` `%s`",
+                                                    proposal,
+                                                    expiration
+                                                )
+                                            )
+                                        );
+                                    }
+                                    return CompletableFuture.allOf();
+                                }
+                            )
+                    )
+                    .toArray(CompletableFuture[]::new)
+            )
         );
     }
 
@@ -122,6 +144,17 @@ final class Proposals {
      */
     private Key proposalKey(final String uuid) {
         return new Key.From(new RootKey(this.target), uuid);
+    }
+
+    /**
+     * Checks that instant in string format is not expired, e.g. is after current time.
+     * Empty string considered to never expire.
+     *
+     * @param instant Instant in string format.
+     * @return True if instant is not expired, false - otherwise.
+     */
+    private static boolean isNotExpired(final String instant) {
+        return instant.isEmpty() || Instant.parse(instant).isAfter(Instant.now());
     }
 
     /**
