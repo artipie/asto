fab5c321b9c000c21e88c3c0c8478b88d7aba335
olenagerasimova <olena.gerasimova@gmail.com>
2020-11-05 14:19:32 +0300

    #286 - Added Remote and implementation to handle errors

---
+++ src/main/java/com/artipie/asto/cache/Remote.java
@@ -0,0 +1,80 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2020 artipie.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.artipie.asto.cache;
+
+import com.artipie.asto.Content;
+import com.jcabi.log.Logger;
+import java.util.Optional;
+import java.util.concurrent.CompletionStage;
+import java.util.function.Supplier;
+
+/**
+ * Async {@link java.util.function.Supplier} of {@link java.util.concurrent.CompletionStage}
+ * with {@link Optional} of {@link Content}. It's a {@link FunctionalInterface}.
+ *
+ * @since 0.4
+ */
+@FunctionalInterface
+public interface Remote extends Supplier<CompletionStage<Optional<? extends Content>>> {
+
+    @Override
+    CompletionStage<Optional<? extends Content>> get();
+
+    /**
+     * Implementation of {@link Remote} that handle all possible errors and returns
+     * empty {@link Optional} if any exception happened.
+     * @since 0.4
+     */
+    class WithErrorHandling implements Remote {
+
+        /**
+         * Origin.
+         */
+        private final Remote origin;
+
+        /**
+         * Ctor.
+         * @param origin Origin
+         */
+        public WithErrorHandling(final Remote origin) {
+            this.origin = origin;
+        }
+
+        @Override
+        public CompletionStage<Optional<? extends Content>> get() {
+            return this.origin.get().handle(
+                (content, throwable) -> {
+                    final Optional<? extends Content> res;
+                    if (throwable == null) {
+                        res = content;
+                    } else {
+                        Logger.error(this.origin.getClass(), throwable.getMessage());
+                        res = Optional.empty();
+                    }
+                    return res;
+                }
+            );
+        }
+    }
+}
---
+++ src/test/java/com/artipie/asto/cache/RemoteWithErrorHandlingTest.java
@@ -0,0 +1,66 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2020 artipie.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.artipie.asto.cache;
+
+import com.artipie.asto.Content;
+import com.artipie.asto.FailedCompletionStage;
+import com.artipie.asto.ext.PublisherAs;
+import java.net.ConnectException;
+import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.core.IsEqual;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Test for {@link Remote.WithErrorHandling}.
+ * @since 0.4
+ */
+class RemoteWithErrorHandlingTest {
+
+    @Test
+    void returnsContentFromOrigin() {
+        final byte[] bytes = "123".getBytes();
+        MatcherAssert.assertThat(
+            new PublisherAs(
+                new Remote.WithErrorHandling(
+                    () -> CompletableFuture.completedFuture(
+                        Optional.of(new Content.From(bytes))
+                    )
+                ).get().toCompletableFuture().join().get()
+            ).bytes().toCompletableFuture().join(),
+            new IsEqual<>(bytes)
+        );
+    }
+
+    @Test
+    void returnsEmptyOnError() {
+        MatcherAssert.assertThat(
+            new Remote.WithErrorHandling(
+                () -> new FailedCompletionStage<>(new ConnectException("Connection error"))
+            ).get().toCompletableFuture().join().isPresent(),
+            new IsEqual<>(false)
+        );
+    }
+}
