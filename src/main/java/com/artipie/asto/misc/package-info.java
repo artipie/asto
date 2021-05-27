50bd299d95c0a236cb8305324f1d386cd79f0b5e
Alena <olena.gerasimova@gmail.com>
2021-05-27 11:58:02 +0300

    Added consumers and functions with `ArtipieException` (#318)
    Closes #317
    Moved UncheckedConsumer and UncheckedFunc from rpm and corrected them to throw ArtipieException, also added specific UncheckedIOConsumer and UncheckedIOFunc for IO errors.

---
+++ src/main/java/com/artipie/asto/misc/package-info.java
@@ -0,0 +1,11 @@
+/*
+ * The MIT License (MIT) Copyright (c) 2020-2021 artipie.com
+ * https://github.com/artipie/asto/LICENSE.txt
+ */
+
+/**
+ * Misc tools.
+ *
+ * @since 1.2
+ */
+package com.artipie.asto.misc;
---
+++ src/test/java/com/artipie/asto/misc/package-info.java
@@ -0,0 +1,11 @@
+/*
+ * The MIT License (MIT) Copyright (c) 2020-2021 artipie.com
+ * https://github.com/artipie/asto/LICENSE.txt
+ */
+
+/**
+ * Misc tools tests.
+ *
+ * @since 1.1
+ */
+package com.artipie.asto.misc;
