0e139addbb54777ef5f9cc2bfd4fa737882890ce
Alexander <38591972+genryxy@users.noreply.github.com>
2021-05-31 10:52:07 +0300

    Add implementation of  `save` and `value` for benchmark storage (#315)
    Part of #314
    Added implementation of save and value for benchmark storage.
    Later add more tests for this implementation and implement other methods.

---
+++ src/main/java/com/artipie/asto/memory/BenchmarkStorage.java
@@ -0,0 +1,140 @@
+/*
+ * The MIT License (MIT) Copyright (c) 2020-2021 artipie.com
+ * https://github.com/artipie/asto/LICENSE.txt
+ */
+package com.artipie.asto.memory;
+
+import com.artipie.asto.ArtipieIOException;
+import com.artipie.asto.Concatenation;
+import com.artipie.asto.Content;
+import com.artipie.asto.FailedCompletionStage;
+import com.artipie.asto.Key;
+import com.artipie.asto.OneTimePublisher;
+import com.artipie.asto.Remaining;
+import com.artipie.asto.Storage;
+import com.artipie.asto.ValueNotFoundException;
+import com.artipie.asto.ext.CompletableFutureSupport;
+import hu.akarnokd.rxjava2.interop.SingleInterop;
+import io.vavr.NotImplementedError;
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentSkipListMap;
+import java.util.function.Function;
+
+/**
+ * Storage implementation for benchmarks.
+ * @since 1.1.0
+ * @checkstyle ClassDataAbstractionCouplingCheck (500 lines)
+ */
+@SuppressWarnings("PMD.AvoidDuplicateLiterals")
+public final class BenchmarkStorage implements Storage {
+    /**
+     * Backend storage.
+     */
+    private final InMemoryStorage backend;
+
+    /**
+     * Local storage.
+     */
+    private final Map<Key, byte[]> local;
+
+    /**
+     * Set which contains deleted keys.
+     */
+    private final Set<Key> deleted;
+
+    /**
+     * Ctor.
+     * @param backend Backend storage
+     */
+    public BenchmarkStorage(final InMemoryStorage backend) {
+        this.backend = backend;
+        this.local = new ConcurrentSkipListMap<>(Key.CMPRTR_STRING);
+        this.deleted = ConcurrentHashMap.newKeySet();
+    }
+
+    @Override
+    public CompletableFuture<Boolean> exists(final Key key) {
+        throw new NotImplementedError("Not implemented yet");
+    }
+
+    @Override
+    public CompletableFuture<Collection<Key>> list(final Key prefix) {
+        throw new NotImplementedError("Not implemented yet");
+    }
+
+    @Override
+    public CompletableFuture<Void> save(final Key key, final Content content) {
+        final CompletableFuture<Void> res;
+        if (Key.ROOT.equals(key)) {
+            res = new CompletableFutureSupport.Failed<Void>(
+                new ArtipieIOException("Unable to save to root")
+            ).get();
+        } else {
+            res = new Concatenation(new OneTimePublisher<>(content)).single()
+                .to(SingleInterop.get())
+                .thenApply(Remaining::new)
+                .thenApply(Remaining::bytes)
+                .thenAccept(bytes -> this.local.put(key, bytes))
+                .thenAccept(noth -> this.deleted.remove(key))
+                .toCompletableFuture();
+        }
+        return res;
+    }
+
+    @Override
+    public CompletableFuture<Void> move(final Key source, final Key destination) {
+        throw new NotImplementedError("Not implemented yet");
+    }
+
+    @Override
+    public CompletableFuture<Long> size(final Key key) {
+        throw new NotImplementedError("Not implemented yet");
+    }
+
+    @Override
+    public CompletableFuture<Content> value(final Key key) {
+        // @checkstyle NestedIfDepthCheck (30 lines)
+        final CompletionStage<Content> res;
+        if (Key.ROOT.equals(key)) {
+            res = new FailedCompletionStage<>(new ArtipieIOException("Unable to load from root"));
+        } else {
+            if (this.deleted.contains(key)) {
+                res = new FailedCompletionStage<>(new ValueNotFoundException(key));
+            } else {
+                final byte[] lcl = this.local.computeIfAbsent(
+                    key, ckey -> this.backend.data.get(ckey.string())
+                );
+                if (lcl == null) {
+                    res = new FailedCompletionStage<>(new ValueNotFoundException(key));
+                } else {
+                    if (this.deleted.contains(key)) {
+                        res = new FailedCompletionStage<>(new ValueNotFoundException(key));
+                    } else {
+                        res = CompletableFuture.completedFuture(
+                            new Content.OneTime(new Content.From(lcl))
+                        );
+                    }
+                }
+            }
+        }
+        return res.toCompletableFuture();
+    }
+
+    @Override
+    public CompletableFuture<Void> delete(final Key key) {
+        throw new NotImplementedError("Not implemented yet");
+    }
+
+    @Override
+    public <T> CompletionStage<T> exclusively(
+        final Key key,
+        final Function<Storage, CompletionStage<T>> operation
+    ) {
+        throw new NotImplementedError("Not implemented yet");
+    }
+}
---
+++ src/test/java/com/artipie/asto/memory/BenchmarkStorageTest.java
@@ -0,0 +1,89 @@
+/*
+ * The MIT License (MIT) Copyright (c) 2020-2021 artipie.com
+ * https://github.com/artipie/asto/LICENSE.txt
+ */
+package com.artipie.asto.memory;
+
+import com.artipie.asto.Content;
+import com.artipie.asto.Key;
+import com.artipie.asto.ext.PublisherAs;
+import java.util.NavigableMap;
+import java.util.TreeMap;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.core.IsEqual;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Test for {@link BenchmarkStorage}.
+ * @since 1.1.0
+ * @checkstyle ClassDataAbstractionCouplingCheck (500 lines)
+ */
+@SuppressWarnings("PMD.AvoidDuplicateLiterals")
+final class BenchmarkStorageTest {
+    @Test
+    void obtainsValueFromBackendIfAbsenceInLocal() {
+        final Key key = new Key.From("somekey");
+        final byte[] data = "some data".getBytes();
+        final NavigableMap<String, byte[]> backdata = new TreeMap<>();
+        backdata.put(key.string(), data);
+        final InMemoryStorage memory = new InMemoryStorage(backdata);
+        final BenchmarkStorage bench = new BenchmarkStorage(memory);
+        MatcherAssert.assertThat(
+            this.valueFrom(bench, key),
+            new IsEqual<>(data)
+        );
+    }
+
+    @Test
+    void obtainsValueFromLocalWithEmptyBackend() {
+        final Key key = new Key.From("somekey");
+        final byte[] data = "some data".getBytes();
+        final BenchmarkStorage bench = new BenchmarkStorage(new InMemoryStorage());
+        bench.save(key, new Content.From(data)).join();
+        bench.save(new Key.From("another"), Content.EMPTY).join();
+        MatcherAssert.assertThat(
+            this.valueFrom(bench, key),
+            new IsEqual<>(data)
+        );
+    }
+
+    @Test
+    void obtainsValueFromLocalWhenInLocalAndBackedIsPresent() {
+        final Key key = new Key.From("somekey");
+        final byte[] lcl = "some local data".getBytes();
+        final byte[] back = "some backend data".getBytes();
+        final NavigableMap<String, byte[]> backdata = new TreeMap<>();
+        backdata.put(key.string(), back);
+        final BenchmarkStorage bench = new BenchmarkStorage(new InMemoryStorage(backdata));
+        bench.save(key, new Content.From(lcl)).join();
+        MatcherAssert.assertThat(
+            this.valueFrom(bench, key),
+            new IsEqual<>(lcl)
+        );
+    }
+
+    @Test
+    void savesOnlyInLocal() {
+        final InMemoryStorage memory = new InMemoryStorage();
+        final BenchmarkStorage bench = new BenchmarkStorage(memory);
+        final Key key = new Key.From("somekey");
+        final byte[] data = "should save in local".getBytes();
+        bench.save(key, new Content.From(data)).join();
+        MatcherAssert.assertThat(
+            "Value was not saved in local storage",
+            this.valueFrom(bench, key),
+            new IsEqual<>(data)
+        );
+        MatcherAssert.assertThat(
+            "Value was saved in backend storage",
+            memory.exists(key).join(),
+            new IsEqual<>(false)
+        );
+    }
+
+    private byte[] valueFrom(final BenchmarkStorage bench, final Key key) {
+        return new PublisherAs(bench.value(key).join())
+            .bytes()
+            .toCompletableFuture().join();
+    }
+}
