4f5093d9e3f6545e26bb4d9a6fc1313540613d9f
olenagerasimova <olena.gerasimova@gmail.com>
2021-06-16 19:06:01 +0300

    Added unchecked scalars and test

---
+++ src/main/java/com/artipie/asto/misc/UncheckedScalar.java
@@ -0,0 +1,58 @@
+/*
+ * The MIT License (MIT) Copyright (c) 2020-2021 artipie.com
+ * https://github.com/artipie/asto/LICENSE.txt
+ */
+package com.artipie.asto.misc;
+
+import com.artipie.ArtipieException;
+import org.cactoos.Scalar;
+
+/**
+ * Scalar that throws {@link com.artipie.ArtipieException} on error.
+ * @param <T> Return value type
+ * @param <E> Error type
+ * @since 1.3
+ */
+public final class UncheckedScalar<T, E extends Exception> implements Scalar<T> {
+
+    /**
+     * Original origin.
+     */
+    private final Checked<T, E> origin;
+
+    /**
+     * Ctor.
+     * @param origin Encapsulated origin
+     */
+    public UncheckedScalar(final Checked<T, E> origin) {
+        this.origin = origin;
+    }
+
+    @Override
+    @SuppressWarnings("PMD.AvoidCatchingGenericException")
+    public T value() {
+        try {
+            return this.origin.value();
+        // @checkstyle IllegalCatchCheck (1 line)
+        } catch (final Exception ex) {
+            throw new ArtipieException(ex);
+        }
+    }
+
+    /**
+     * Checked version of scalar.
+     * @param <R> Return type
+     * @param <E> Error type
+     * @since 1.1
+     */
+    @FunctionalInterface
+    public interface Checked<R, E extends Exception> {
+
+        /**
+         * Return value.
+         * @return Result
+         * @throws E On error
+         */
+        R value() throws E;
+    }
+}
