547d3767daac68bbdc09ba2cb2b5ac318d6473fd
Olivier B. OURA <baudoliver7@gmail.com>
2021-11-12 10:33:17 +0000

    Key that excludes with index #352

---
+++ src/main/java/com/artipie/asto/key/KeyExcludeByIndex.java
@@ -0,0 +1,43 @@
+/*
+ * The MIT License (MIT) Copyright (c) 2020-2021 artipie.com
+ * https://github.com/artipie/asto/LICENSE.txt
+ */
+
+package com.artipie.asto.key;
+
+import com.artipie.asto.Key;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Key that excludes a part by its index.
+ *
+ * @since 1.9.1
+ */
+public final class KeyExcludeByIndex extends Key.Wrap {
+
+    /**
+     * Ctor.
+     * @param key Key
+     * @param index Index of part
+     */
+    public KeyExcludeByIndex(final Key key, final int index) {
+        super(
+            new From(KeyExcludeByIndex.exclude(key, index))
+        );
+    }
+
+    /**
+     * Excludes part by its index.
+     * @param key Key
+     * @param index Index of part to exclude
+     * @return List of parts
+     */
+    private static List<String> exclude(final Key key, final int index) {
+        final List<String> parts = new LinkedList<>(key.parts());
+        if (index >= 0 && index < parts.size()) {
+            parts.remove(index);
+        }
+        return parts;
+    }
+}
