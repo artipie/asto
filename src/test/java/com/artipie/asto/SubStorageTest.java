aee483fca26b2506e3d3ed2d24f8ddbff97da4f8
Olivier Baudouin OURA <baudoliver7@gmail.com>
2021-11-16 13:24:02 +0000

    test: add  tests for SubStorage (#366)
    Test methods `size`, `move` and `save`.
    Add a puzzle to continue to add more tests
    
    Close: #354

--- src/test/java/com/artipie/asto/SubStorageTest.java
+++ src/test/java/com/artipie/asto/SubStorageTest.java
@@ -17,9 +17,9 @@ import org.junit.jupiter.params.provider.ValueSource;
 /**
  * Test for {@link SubStorage}.
  * @since 1.9
- * @todo #351:30min Add more tests for {@link SubStorage}: all the methods of the class should be
- *  verified, do not forget to add tests with different prefixes,
- *  including {@link Key#ROOT} as prefix.
+ * @todo #352:30min Continue to add more tests for {@link SubStorage}.
+ *  All the methods of the class should be verified, do not forget to
+ *  add tests with different prefixes, including {@link Key#ROOT} as prefix.
  */
 @SuppressWarnings("PMD.AvoidDuplicateLiterals")
 class SubStorageTest {
@@ -104,6 +104,75 @@ class SubStorageTest {
         MatcherAssert.assertThat(
             "Returns true with ROOT prefix when item exists",
             new SubStorage(Key.ROOT, this.asto).exists(new Key.From(prefix, "any.txt")).join()
+        );
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"my-project", "com/example"})
+    void savesContent(final String pref) {
+        final Key prefix = new Key.From(pref);
+        final byte[] data = "some data".getBytes(StandardCharsets.UTF_8);
+        final SubStorage substo = new SubStorage(prefix, this.asto);
+        substo.save(new Key.From("package"), new Content.From(data)).join();
+        MatcherAssert.assertThat(
+            "Returns storage item with prefix",
+            new BlockingStorage(this.asto).value(new Key.From(prefix, "package")),
+            new IsEqual<>(data)
+        );
+        MatcherAssert.assertThat(
+            "Returns storage item with ROOT prefix",
+            new BlockingStorage(new SubStorage(Key.ROOT, this.asto))
+                .value(new Key.From(prefix, "package")),
+            new IsEqual<>(data)
+        );
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"my-project", "com/example"})
+    void movesContent(final String pref) {
+        final Key prefix = new Key.From(pref);
+        final byte[] data = "source".getBytes(StandardCharsets.UTF_8);
+        final Key source = new Key.From("src");
+        final Key destination = new Key.From("dest");
+        this.asto.save(new Key.From(prefix, source), new Content.From(data)).join();
+        this.asto.save(
+            new Key.From(prefix, destination),
+            new Content.From("destination".getBytes(StandardCharsets.UTF_8))
+        ).join();
+        final SubStorage substo = new SubStorage(prefix, this.asto);
+        substo.move(source, destination).join();
+        MatcherAssert.assertThat(
+            "Moves key value with prefix",
+            new BlockingStorage(this.asto).value(new Key.From(prefix, destination)),
+            new IsEqual<>(data)
+        );
+        MatcherAssert.assertThat(
+            "Moves key value with ROOT prefix",
+            new BlockingStorage(new SubStorage(Key.ROOT, this.asto))
+                .value(new Key.From(prefix, destination)),
+            new IsEqual<>(data)
+        );
+    }
+
+    @SuppressWarnings("deprecation")
+    @ParameterizedTest
+    @ValueSource(strings = {"url", "sub/url"})
+    void readsSize(final String pref) {
+        final Key prefix = new Key.From(pref);
+        final byte[] data = "012004407".getBytes(StandardCharsets.UTF_8);
+        final Long datalgt = Long.valueOf(data.length);
+        final Key keyres = new Key.From("resource");
+        this.asto.save(new Key.From(prefix, keyres), new Content.From(data)).join();
+        MatcherAssert.assertThat(
+            "Gets value size with prefix",
+            new BlockingStorage(new SubStorage(prefix, this.asto)).size(keyres),
+            new IsEqual<>(datalgt)
+        );
+        MatcherAssert.assertThat(
+            "Gets value size with ROOT prefix",
+            new BlockingStorage(new SubStorage(Key.ROOT, this.asto))
+                .size(new Key.From(prefix, keyres)),
+            new IsEqual<>(datalgt)
         );
     }
 }
