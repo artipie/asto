d391b7445f32ab05b8140ee063684691e5fcf780
Olivier B. OURA <baudoliver7@gmail.com>
2021-11-17 12:48:29 +0000

    Improves test for deleteAll method #348

--- src/test/java/com/artipie/asto/StorageDeleteAllTest.java
+++ src/test/java/com/artipie/asto/StorageDeleteAllTest.java
@@ -6,8 +6,8 @@ package com.artipie.asto;
 
 import com.artipie.asto.blocking.BlockingStorage;
 import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
 import org.hamcrest.core.IsEqual;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.TestTemplate;
 import org.junit.jupiter.api.extension.ExtendWith;
 
@@ -15,26 +15,37 @@ import org.junit.jupiter.api.extension.ExtendWith;
  * Tests for {@link Storage#deleteAll(Key)}.
  *
  * @since 1.9.1
- * @todo #348:30min Fix error when trying to save data with prefixed Key.
- *  Test below goes in error when we try to save data in {@link com.artipie.asto.fs.FileStorage}
- *  and {@link com.artipie.asto.fs.VertxFileStorage} with prefixed key, after saving data with
- *  key prefix. Please, fix it and enable this test.
  */
 @SuppressWarnings("PMD.AvoidDuplicateLiterals")
 @ExtendWith(StorageExtension.class)
 public final class StorageDeleteAllTest {
 
-    @Disabled
     @TestTemplate
     void shouldDeleteAllItemsWithKeyPrefix(final Storage storage) {
-        final Key prefix = new Key.From("one");
-        storage.save(prefix, Content.EMPTY).join();
+        final Key prefix = new Key.From("a");
+        storage.save(new Key.From(prefix, "one"), Content.EMPTY).join();
         storage.save(new Key.From(prefix, "two"), Content.EMPTY).join();
-        storage.save(new Key.From(prefix, "two", "three"), Content.EMPTY).join();
+        storage.save(new Key.From("b", "three"), Content.EMPTY).join();
+        storage.save(new Key.From("four"), Content.EMPTY).join();
         final BlockingStorage blocking = new BlockingStorage(storage);
         blocking.deleteAll(prefix);
         MatcherAssert.assertThat(
+            "Should not have items with key prefix",
             blocking.list(prefix).size(),
+            new IsEqual<>(0)
+        );
+        MatcherAssert.assertThat(
+            "Should have other items",
+            blocking.list(Key.ROOT),
+            Matchers.hasItems(
+                new Key.From("b", "three"),
+                new Key.From("four")
+            )
+        );
+        blocking.deleteAll(Key.ROOT);
+        MatcherAssert.assertThat(
+            "Should remove all items",
+            blocking.list(Key.ROOT).size(),
             new IsEqual<>(0)
         );
     }
