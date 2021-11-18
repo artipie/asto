6ee80f6d136a8f42d3a3fe7821cd534134d2535d
olenagerasimova <olena.gereasimova@gmail.com>
2021-11-18 16:27:53 +0300

    Compare keys by string representation

--- src/main/java/com/artipie/asto/fs/FileStorage.java
+++ src/main/java/com/artipie/asto/fs/FileStorage.java
@@ -201,7 +201,7 @@ public final class FileStorage implements Storage {
     @Override
     public CompletableFuture<Content> value(final Key key) {
         final CompletableFuture<Content> res;
-        if (Key.ROOT.equals(key)) {
+        if (Key.ROOT.string().equals(key.string())) {
             res = new CompletableFutureSupport.Failed<Content>(
                 new ArtipieIOException("Unable to load from root")
             ).get();
