04f56c20296b16ac92bc235c8b8921bfb538573c
Denis Garus <garus.d.g@gmail.com>
2022-08-31 11:55:35 +0300

    Create `StorageWhiteboxVerification` test class in the src folder (#465)
    * Move common storage tests to one class in src folder
    
    * fix review comments
    
    * fix licenses

---
+++ asto-core/src/test/java/com/artipie/asto/VertxFileStorageVerificationTest.java
@@ -0,0 +1,72 @@
+/*
+ * The MIT License (MIT) Copyright (c) 2020-2022 artipie.com
+ * https://github.com/artipie/asto/LICENSE.txt
+ */
+package com.artipie.asto;
+
+import com.artipie.asto.fs.VertxFileStorage;
+import com.artipie.asto.test.StorageWhiteboxVerification;
+import io.vertx.reactivex.core.Vertx;
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Optional;
+import java.util.stream.Stream;
+import org.junit.After;
+import org.junit.Before;
+
+/**
+ * Vertx file storage verification test.
+ *
+ * @checkstyle ProtectedMethodInFinalClassCheck (500 lines)
+ * @since 1.14.0
+ */
+@SuppressWarnings("PMD.TestClassWithoutTestCases")
+public final class VertxFileStorageVerificationTest extends StorageWhiteboxVerification {
+
+    /**
+     * Temp dir.
+     */
+    private Path temp;
+
+    /**
+     * Vert.x file System.
+     */
+    private Vertx vertx;
+
+    @Before
+    public void setUp() throws Exception {
+        this.vertx = Vertx.vertx();
+        this.temp = Files.createTempDirectory("vtx_junit");
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        this.vertx.close();
+        try (Stream<Path> walk = Files.walk(this.temp)) {
+            walk.map(Path::toFile)
+                .forEach(File::delete);
+        }
+    }
+
+    @Override
+    protected Storage newStorage() throws Exception {
+        return new VertxFileStorage(
+            this.temp.resolve("base"),
+            this.vertx
+        );
+    }
+
+    @Override
+    protected Optional<Storage> newBaseForRootSubStorage() {
+        return Optional.empty();
+    }
+
+    @Override
+    protected Optional<Storage> newBaseForSubStorage() {
+        return Optional.of(
+            new VertxFileStorage(this.temp.resolve("sub-storage"), this.vertx)
+        );
+    }
+
+}
