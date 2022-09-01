392f8890b2eb6c1762717b129c7009be340b7deb
Denis Garus <garus.d.g@gmail.com>
2022-09-01 16:09:24 +0300

    VertxFileStorage as separate module (#468)
    * Vertx file storage as a separate module

---
+++ asto-vertx-file/src/main/java/com/artipie/asto/fs/package-info.java
@@ -0,0 +1,11 @@
+/*
+ * The MIT License (MIT) Copyright (c) 2020-2022 artipie.com
+ * https://github.com/artipie/asto/LICENSE.txt
+ */
+
+/**
+ * Vertx file system implementation of asto.
+ *
+ * @since 0.1
+ */
+package com.artipie.asto.fs;
---
+++ asto-vertx-file/src/test/java/com/artipie/asto/package-info.java
@@ -0,0 +1,12 @@
+/*
+ * The MIT License (MIT) Copyright (c) 2020-2022 artipie.com
+ * https://github.com/artipie/asto/LICENSE.txt
+ */
+
+/**
+ * Vertx file system implementation of asto tests.
+ *
+ * @since 0.1
+ */
+package com.artipie.asto;
+
